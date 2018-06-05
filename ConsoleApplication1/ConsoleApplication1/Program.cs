9f567a4db4dd8323b2c04c23cc8edf60b2f99399
Ninad Madhab <32309032+FalconMadhab@users.noreply.github.com>
2018-06-05 15:59:15 +0530

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Limilabs.Client.IMAP;
+using Limilabs.Mail;
+using Limilabs.Mail.MIME;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Imap imap = new Imap())
+            {
+                imap.ConnectSSL("imap.example.com");
+                imap.Login("example@example.com","p@assword");
+                imap.SelectInbox();
+                List<long> uids = imap.SearchFlag(Flag.Unseen);
+                foreach (long uid in uids)
+                {
+                    IMail email= new MailBuilder().CreateFromEml(imap.GetMessageByUID(uid)) ;
+                    Console.WriteLine(email.Subject);
+                    
+                    // save all attachments to disk
+		            foreach(MimeData mime in email.Attachments)
+		            {
+                        Console.WriteLine(mime.FileName);
+			            mime.Save("c:\\Your desired path\\" + mime.FileName);
+		            }
+	            }
+	            imap.Close();
+                }
+            }
+
+        }
+    }
